e3aee4b85f2200fa33eafaed648858ad14980f0b
donluispanis <lga42@gcloud.ua.es>
2018-08-13 20:59:06 +0200

    Alexei mirame

--- Source/Artemisa/ArtemisaPawn.cpp
+++ Source/Artemisa/ArtemisaPawn.cpp
@@ -32,22 +32,22 @@ AArtemisaPawn::AArtemisaPawn()
 	static ConstructorHelpers::FObjectFinder<USoundBase> FireAudio(TEXT("/Game/TwinStick/Audio/shoot.shoot"));
 	FireSound = FireAudio.Object;
 
-	// Create a camera boom...
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->bAbsoluteRotation = true; // Don't want arm to rotate when ship does
-	CameraBoom->TargetArmLength = 1200.f;
-	CameraBoom->RelativeRotation = FRotator(-80.f, 0.f, 0.f);
-	CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
+	// Create a spring arm component
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetRelativeLocation(FVector(0.0f, 0.0f, 90));
+	SpringArm->SetWorldRotation(FRotator(-90.0f, 0.0f, 0.0f));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 2000;
+	SpringArm->bEnableCameraLag = false;
+	SpringArm->bEnableCameraRotationLag = false;
+	SpringArm->bInheritPitch = true;
+	SpringArm->bInheritYaw = true;
+	SpringArm->bInheritRoll = true;
 
-	// Create a camera...
-	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TopDownCamera"));
-	CameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
-	//CameraComponent->SetupAttachment(RootComponent);
-	CameraComponent->bUsePawnControlRotation = false;	// Camera does not rotate relative to arm
-	/*CameraComponent->bAbsoluteRotation = true;        // Camera has to be absolute so calculations work and it doesn't rotate with the camera
-	CameraComponent->bAbsoluteLocation = true;        // Camera has to be absolute so calculations work AND IT MOVES RIGHT
-	*/
+	// Create camera component 
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera0"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);	// Attach the camera
+	Camera->bUsePawnControlRotation = false; // Don't rotate camera with controller
 
 	//Create collision component
 	collisionRadius = 50.f;
@@ -182,11 +182,14 @@ void AArtemisaPawn::MoveOnPlanet(float deltaTime)
 	//=======================================================
 	//Location
 	// Calculus: "Absolute position of object" where the ship is + normal from surface * distance + (200m behind = - forward vector * 200.f * distance desired from ship)
-	CameraBoom->SetRelativeLocation(FVector(0.0f, 0.0f, 1.0f) * planet->GetActorScale3D().X * camera_ratio - FVector(horizontal_distance_camera, 0.0f, 0.0f));
+	//CameraBoom->SetRelativeLocation(FVector(0.0f, 0.0f, 1.0f) * planet->GetActorScale3D().X * camera_ratio - FVector(horizontal_distance_camera, 0.0f, 0.0f));
+	//FVector normal = planet->GetActorLocation() - this->GetActorLocation();
+	//normal.Normalize();
+	//CameraBoom->SetRelativeLocation(normal * 2000);
 
 	//Rotation
-	FRotator lookAt = UKismetMathLibrary::FindLookAtRotation(CameraBoom->GetComponentLocation(), GetActorLocation());
-	CameraBoom->SetRelativeRotation(lookAt);
+	//FRotator lookAt = UKismetMathLibrary::FindLookAtRotation(CameraBoom->GetComponentLocation(), GetActorLocation());
+	//CameraBoom->SetRelativeRotation(lookAt);
 
 	
 
