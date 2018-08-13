176a9da4f421d8c2926fbe6417461f0733d7e3f5
Alexei-117 <alexei-117@hotmail.com>
2018-08-13 14:59:23 +0200

    Subiendo lo que hay que lo q no hay no se sube

--- Source/Artemisa/EnemySpawn.h
+++ Source/Artemisa/EnemySpawn.h
@@ -4,13 +4,18 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Planet.h"
 #include "EnemySpawn.generated.h"
 
 UCLASS()
 class ARTEMISA_API AEnemySpawn : public AActor
 {
 	GENERATED_BODY()
-	
+
+	//Private variables
+	float remaining_time;
+	int remaining_enemies;
+
 public:	
 	// Sets default values for this actor's properties
 	AEnemySpawn();
@@ -23,6 +28,26 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	//Spawn at a specific location and a number of enemies
+	void Spawn(int);
 	
-	
+	//SPAWN ACTOR DATA
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Planet)
+	float spawn_frecuency;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Planet)
+	int enemies_to_spawn;
+
+	//PLANET DATA
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Planet)
+		APlanet* planet;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Planet)
+	float enemy_distance_ratio;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Planet)
+	float spawn_distance_ratio;
+
+	/** Handle for efficient management of ShotTimerExpired timer */
+	FTimerHandle TimerHandle_ShotTimerExpired;
 };
